68826202b9fc20c07417b21c2cd0c165a6c7ad7c
Stephen Carlson <stcarlso@gmail.com>
2015-07-03 15:23:57 -0700

    Created HABService, the headless background task which logs configured sensor data to a log file in the C:\Users\DefaultAccount\Downloads folder on the Raspberry PI 2. Currently supports telemetry MCU major 2 and HTU21D digital I2C temperature/humidity sensor.

---
+++ stcarlso/HABService/HABService/HTU21I2CSensor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Devices.I2c;
+
+namespace HABService {
+	class HTU21I2CSensor : I2CSensor {
+		/// <summary>
+		/// Conversion for relative humidity = 125 / 65536 (datasheet pg. 15)
+		/// </summary>
+		private const double HUMID_FACTOR = 0.0019073486328125;
+		/// <summary>
+		/// Humidity offset (datasheet pg. 15)
+		/// This allows -6 and +118% RH, as noted in datasheet. We will not clamp the values,
+		/// as cutting the junk data can be done in post processing, and we want to see if
+		/// this data actually appears in the log.
+		/// </summary>
+		private const double HUMID_OFFSET = -6.00;
+		/// <summary>
+		/// Measures temperature, no hold master (datasheet pg. 11)
+		/// </summary>
+		private const int MEASURE_TEMP = 0xF3;
+		/// <summary>
+		/// Measures humidity, no hold master (datasheet pg. 11)
+		/// </summary>
+		private const int MEASURE_HUMID = 0xF5;
+		/// <summary>
+		/// Conversion for temperature = 175.72 / 65536 (datasheet pg. 15)
+		/// </summary>
+		private const double TEMP_FACTOR = 0.0026812744140625;
+		/// <summary>
+		/// Temperature offset (datasheet pg. 15)
+		/// </summary>
+		private const double TEMP_OFFSET = -46.85;
+
+		public override int Address {
+			get {
+				// Datasheet page 10
+				return 0x40;
+			}
+		}
+		/// <summary>
+		/// Temperature follows continuity principles and should not change too rapidly. Once
+		/// per second is adequate.
+		/// </summary>
+		public override int LogInterval {
+			get {
+				return 1000;
+			}
+		}
+
+		// HTU sensor address cannot be configured in hardware or software
+		public HTU21I2CSensor() { }
+		public override string Init(I2cDevice device) {
+			// Trigger a soft reset which will wipe the registers to defaults
+			// This means that resolution is 12-bit RH/14-bit Temperature
+			device.Write(new byte[] { 0xFE });
+			return "TEMP_C,REL_HUMID";
+		}
+		/// <summary>
+		/// Measures data from the HTU21D.
+		/// </summary>
+		/// <param name="device">The I2C device</param>
+		/// <param name="command">MEASURE_TEMP or MEASURE_HUMIDITY</param>
+		/// <returns>The measured value in quids FS</returns>
+		private async Task<int> Measure(I2cDevice device, int command) {
+			byte[] data = new byte[3];
+			// No Hold Master mode
+			device.Write(new byte[] { (byte)command });
+			await Task.Delay(30);
+			// Max measurement time is 50 ms for temperature and 16 ms for humidity
+			for (int i = 0; i < 3; i++) {
+				await Task.Delay(15);
+				try {
+					device.Read(data);
+					// Data is MSB-first in the first 2 bytes
+					int value = data[1] & 0xFC;
+					return value | ((data[0] & 0xFF) << 8);
+				} catch (IOException) {
+					// Swallow the NACK and try again in 10 ms
+				}
+			}
+			throw new IOException("Still measuring, increase delay!");
+		}
+		public override async Task<string> Sample(I2cDevice device) {
+			// Measure the data we need
+			double temp = (await Measure(device, MEASURE_TEMP)) * TEMP_FACTOR + TEMP_OFFSET;
+			double humid = (await Measure(device, MEASURE_HUMID)) * HUMID_FACTOR +
+				HUMID_OFFSET;
+			return String.Format("{0:F2},{1:F2}", temp, humid);
+		}
+	}
+}
---
+++ stcarlso/HABService/HABService/TelemetryI2CSensor.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Devices.I2c;
+
+namespace HABService {
+	/// <summary>
+	/// Interface to the Telemetry MCU to log the GPS data from both devices and the
+	/// ODTS (if configured)
+	/// </summary>
+	class TelemetryI2CSensor : I2CSensor {
+		/// <summary>
+		/// The Tiva C default slave address.
+		/// </summary>
+		public const int SLAVE_ADDRESS = 0x2A;
+		/// <summary>
+		/// The major version compliant with this version.
+		/// </summary>
+		public const int SW_VERSION = 2;
+
+		/// <summary>
+		/// The I2C address of the telemetry MCU.
+		/// </summary>
+		private readonly int address;
+
+		public override int Address {
+			get {
+				return address;
+			}
+		}
+		/// <summary>
+		/// Checking this happens at 2Hz. Since Copernicus only returns at 1Hz there is no
+		/// reason to go any faster, the 2x is just to mitigate phasing errors.
+		/// </summary>
+		public override int LogInterval {
+			get {
+				return 500;
+			}
+		}
+
+		/// <summary>
+		/// Create a connection to the default Telemetry MCU address.
+		/// </summary>
+		public TelemetryI2CSensor() : this(SLAVE_ADDRESS) { }
+		/// <summary>
+		/// Create a connection to a custom Telemetry MCU address.
+		/// <param name="address">The custom I2C address</param>
+		/// </summary>
+		public TelemetryI2CSensor(int address) {
+			this.address = address;
+		}
+		/// <summary>
+		/// Displays the GPS data on the screen.
+		/// </summary>
+		/// <param name="device">The I2C device</param>
+		/// <param name="index">0 for Venus, 1 for Copernicus</param>
+		/// <returns>The GPS data in a structure</returns>
+		private GPSData GPSFetch(I2cDevice device, int index) {
+			byte[] data = new byte[17];
+			// Receive 17 bytes starting from 0x10, where 0x30 is the second GPS
+			byte address = (byte)(((index == 0) ? 0x00 : 0x20) + 0x10);
+			device.WriteRead(new byte[] { address }, data);
+			GPSData result = new GPSData();
+			// Convert all to correct types
+			result.latitude = BitConverter.ToInt32(data, 0) * 1E-6;
+			result.longitude = BitConverter.ToInt32(data, 4) * 1E-6;
+			result.velocity = BitConverter.ToInt16(data, 8) * 1E-1;
+			result.heading = BitConverter.ToInt16(data, 10) * 1E-1;
+			result.altitude = BitConverter.ToInt32(data, 12) * 1E-2;
+			result.satellites = data[16];
+			return result;
+		}
+		public override string Init(I2cDevice device) {
+			byte[] data = new byte[3];
+			// Stream read 0x00 (WHO_AM_I), 0x01 (SW_VERSION_MAJOR),
+			// 0x02 (SW_VERSION_MINOR)
+			device.WriteRead(new byte[] { 0x00 }, data);
+			// Check for sync
+			if ((data[0] & 0xFF) != SLAVE_ADDRESS)
+				throw new IOException("Telemetry MCU is not in sync or disconnected");
+			// Check firmware version
+			if ((data[1] & 0xFF) != SW_VERSION)
+				throw new IOException("Telemetry MCU is using incompatible firmware");
+			// Column guide
+			return "FIX_1,LAT_1,LON_1,ALT_1,VEL_1,HDG_1,FIX_2,LAT_2,LON_2,ALT_2,VEL_2,HDG_2";
+		}
+		public override async Task<String> Sample(I2cDevice device) {
+			// NOTE This routine reads and reports both GPSes in the log. It should be trivial
+			// to select which one you really want in post processing. Even at 2 Hz with 256
+			// characters per line and a 20 hour flight, the total file size will be a very
+			// manageable 20 KB.
+			GPSData venus = GPSFetch(device, 0);
+			// Give up VS!
+			await Task.Delay(0);
+			GPSData copernicus = GPSFetch(device, 1);
+			return venus.ToString() + "," + copernicus.ToString();
+		}
+	}
+
+	/// <summary>
+	/// A structure that holds GPS data.
+	/// </summary>
+	struct GPSData {
+		/// <summary>
+		/// Satellite count visible, or 0 if no fix is available.
+		/// </summary>
+		public int satellites;
+		/// <summary>
+		/// Latitude in signed decimal degrees.
+		/// </summary>
+		public double latitude;
+		/// <summary>
+		/// Longitude in signed decimal degrees.
+		/// </summary>
+		public double longitude;
+		/// <summary>
+		/// Velocity over ground in km/h.
+		/// </summary>
+		public double velocity;
+		/// <summary>
+		/// True (i.e. not magnetic) heading made good in degrees (0-360).
+		/// </summary>
+		public double heading;
+		/// <summary>
+		/// Altitude in meters.
+		/// </summary>
+		public double altitude;
+
+		public override string ToString() {
+			return String.Format("{0:D},{1:F6},{2:F6},{3:F1},{4:F1},{5:F1}", satellites,
+				latitude, longitude, altitude, velocity, heading);
+		}
+	}
+}
