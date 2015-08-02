far-far-away-science/hab

commit 6cc2243c3f29c05f8e70cc3ecd13dc9f707b708c

LED changes unintentionally (would have) broken APRS, fix conflict over the PWM system divider

gps-radio-tiva-c/lib-c/startup_rvmdk.S
@@ -86,6 +86,7 @@ __heap_limit
         EXTERN  Uart4IntHandler
         EXTERN  Timer0IntHandler
         EXTERN  Pwm10Handler
+        EXTERN  I2cSlaveHandler
 
 ;******************************************************************************
 ;
@@ -148,7 +149,7 @@ __Vectors
         DCD     IntDefaultHandler           ; SSI1 Rx and Tx
         DCD     IntDefaultHandler           ; Timer 3 subtimer A
         DCD     IntDefaultHandler           ; Timer 3 subtimer B
-        DCD     IntDefaultHandler           ; I2C1 Master and Slave
+        DCD     I2cSlaveHandler             ; I2C1 Master and Slave
         DCD     IntDefaultHandler           ; Quadrature Encoder 1
         DCD     IntDefaultHandler           ; CAN0
         DCD     IntDefaultHandler           ; CAN1

gps-radio-tiva-c/src/i2c.c
@@ -37,6 +37,7 @@
  */
 
 #include "i2c.h"
+#include "signals.h"
 #include <string.h>
 
 #include <driverlib/i2c.h>
@@ -56,7 +57,7 @@ static struct {
     volatile uint8_t address;
 } i2cData;
 
-static void i2cSlaveRequestHandler(void) {
+void I2cSlaveHandler(void) {
     const uint32_t action = MAP_I2CSlaveStatus(I2C_MODULE);
     bool ack = false;
     // Shut off the alarm clock to prevent us from being called again
@@ -81,6 +82,7 @@ static void i2cSlaveRequestHandler(void) {
     {
         // Data has been requested from us
         uint32_t address = (uint32_t)i2cData.address;
+        signalBlue(65535U);
         // Clear data available flag if necessary
         if (address >= REG_LON_0 && address <= REG_HDG_1)
             i2cData.regs[REG_DATA_AVAILABLE] = 0U;
@@ -91,6 +93,7 @@ static void i2cSlaveRequestHandler(void) {
             address = 0U;
         i2cData.address = (uint8_t)address;
         ack = true;
+        signalBlue(0U);
         break;
     }
     default:
@@ -156,7 +159,6 @@ void initializeI2C(void) {
     MAP_I2CSlaveEnable(I2C_MODULE);
     MAP_I2CSlaveInit(I2C_MODULE, I2C_ADDRESS);
     // Register IRQ and clear spurious conditions
-    I2CIntRegister(I2C_MODULE, &i2cSlaveRequestHandler);
     MAP_I2CSlaveIntClear(I2C_MODULE);
     MAP_I2CSlaveIntEnableEx(I2C_MODULE, I2C_SLAVE_INT_DATA);
     // Init register file

gps-radio-tiva-c/src/main.c
@@ -116,10 +116,6 @@ int main()
             shouldSendVenusDataToAprs = !shouldSendVenusDataToAprs;
             startTime = currentTime;
         }
-        
-        // Shut off LED after successful start up (3 seconds)
-        if (currentTime == 3)
-            signalOff();
 
         // TODO if 60 seconds expired write stats to EPPROM
 

gps-radio-tiva-c/src/signals.c
@@ -24,7 +24,6 @@ void initializeSignals(void)
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 #ifdef PWM_OUTPUT
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
-    ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_2);
     ROM_GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
     PWMClockSet(PWM1_BASE, PWM_SYSCLK_DIV_1);
     // Need to set up Generator 2 (M1PWM5=R) and Generator 3 (M1PWM6=B, M1PWM7=G)
@@ -67,7 +66,7 @@ void signalSuccess(void)
 {
 #ifdef PWM_OUTPUT
     signalRed(0U);
-    signalGreen(65535U);
+    signalGreen(1024U);
 #else
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, GPIO_PIN_3);
 #endif
@@ -76,7 +75,7 @@ void signalSuccess(void)
 void signalError(void)
 {
 #ifdef PWM_OUTPUT
-    signalRed(32767U);
+    signalRed(4096U);
     signalGreen(0U);
 #else
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, GPIO_PIN_1);

commit 31674a4155bfb519d68bb12fbe62cd934594c440

Fix bug with I2C interupts not being enabled, introduced by 6cc2243c.

gps-radio-tiva-c/gps-radio-tiva-c.uvoptx
@@ -492,7 +492,7 @@
 
   <Group>
     <GroupName>Libraries</GroupName>
-    <tvExp>1</tvExp>
+    <tvExp>0</tvExp>
     <tvExpOptDlg>0</tvExpOptDlg>
     <cbSel>0</cbSel>
     <RteFlg>0</RteFlg>

gps-radio-tiva-c/gps-radio-tiva-c.uvprojx
@@ -349,8 +349,8 @@
           </ArmAdsMisc>
           <Cads>
             <interw>1</interw>
-            <Optim>1</Optim>
-            <oTime>1</oTime>
+            <Optim>3</Optim>
+            <oTime>0</oTime>
             <SplitLS>0</SplitLS>
             <OneElfS>1</OneElfS>
             <Strict>0</Strict>

gps-radio-tiva-c/lib-c/startup_rvmdk.S
@@ -101,9 +101,9 @@ __Vectors
         DCD     Reset_Handler               ; Reset Handler
         DCD     NmiSR                       ; NMI Handler
         DCD     FaultISR                    ; Hard Fault Handler
-        DCD     IntDefaultHandler           ; The MPU fault handler
-        DCD     IntDefaultHandler           ; The bus fault handler
-        DCD     IntDefaultHandler           ; The usage fault handler
+        DCD     FaultISR                    ; The MPU fault handler
+        DCD     FaultISR                    ; The bus fault handler
+        DCD     FaultISR                    ; The usage fault handler
         DCD     0                           ; Reserved
         DCD     0                           ; Reserved
         DCD     0                           ; Reserved

gps-radio-tiva-c/src/i2c.c
@@ -10,7 +10,9 @@
  * [0x01] - SW_VERSION_MAJOR - returns the major revision defined in i2c.h
  * [0x02] - SW_VERSION_MINOR - returns the minor revision defined in i2c.h
  * [0x03] - DATA_AVAILABLE - returns 1 if data has been updated since last read of any DR, 0 otherwise
- * [0x04-0x0F] - RESERVED
+ * [0x04-0x05] - TEMP - temperature ADC reading in raw counts LSB first
+ * [0x06-0x07] - VOLT - voltage ADC reading in raw counts [TBD convert to voltage once divider set] LSB first
+ * [0x08-0x0F] - RESERVED
  * [0x10-0x13] – LAT 1 - Latitude as signed 32 bit integer in degrees * 1E6 LSB first
  * [0x14-0x17] - LON 1 - Longitude as signed 32 bit integer in degrees * 1E6 LSB first
  * [0x18-0x19] - VEL 1 - Velocity as unsigned 16 bit integer in km/hr * 10 LSB first
@@ -65,6 +67,7 @@ void I2cSlaveHandler(void) {
     switch (action) {
     case I2C_SLAVE_ACT_RREQ_FBR:
     {
+        signalI2CDataRequested();
         // This is the address
         uint32_t newAddress = MAP_I2CSlaveDataGet(I2C_MODULE);
         if (newAddress >= I2C_NUM_REGS)
@@ -76,13 +79,13 @@ void I2cSlaveHandler(void) {
     }
     case I2C_SLAVE_ACT_RREQ:
         // This is not allowed, all registers are currently read only -- ACK but do nothing
+        (void)MAP_I2CSlaveDataGet(I2C_MODULE);
         ack = true;
         break;
     case I2C_SLAVE_ACT_TREQ:
     {
         // Data has been requested from us
         uint32_t address = (uint32_t)i2cData.address;
-        signalI2CDataRequested();
         // Clear data available flag if necessary
         if (address >= REG_LON_0 && address <= REG_HDG_1)
             i2cData.regs[REG_DATA_AVAILABLE] = 0U;
@@ -145,6 +148,23 @@ void submitI2CData(uint32_t index, GpsData *data) {
     MAP_I2CSlaveIntEnableEx(I2C_MODULE, I2C_SLAVE_INT_DATA);
 }
 
+void submitI2CTelemetry(Telemetry *telemetry) {
+    union {
+        uint8_t bytes[2];
+        uint16_t hword;
+    } data16;
+    // Mask I2C interrupts while we update
+    MAP_I2CSlaveIntDisable(I2C_MODULE);
+    // Velocity update
+    data16.hword = (uint16_t)telemetry->cpuTemperature;
+    memcpy(&i2cData.regs[REG_TEMP_0], data16.bytes, sizeof(data16.bytes));
+    // Heading update
+    data16.hword = (uint16_t)telemetry->voltage;
+    memcpy(&i2cData.regs[REG_VOLT_0], data16.bytes, sizeof(data16.bytes));
+    // Restore interrupts
+    MAP_I2CSlaveIntEnableEx(I2C_MODULE, I2C_SLAVE_INT_DATA);
+}
+
 void initializeI2C(void) {
     // Peripheral enable: the I/O port and the I2C module
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -152,15 +172,18 @@ void initializeI2C(void) {
     // Set up pins to I2C mode
     ROM_GPIOPinConfigure(GPIO_PA6_I2C1SCL);
     ROM_GPIOPinConfigure(GPIO_PA7_I2C1SDA);
+    ROM_GPIOPadConfigSet(GPIO_PORTA_BASE, GPIO_PIN_6 | GPIO_PIN_7, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD);
     ROM_GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
     ROM_GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
-    // Set up in slave mode with correct address
-    // NOTE TM4C123 does not have slave functions in its ROM, do not try, you will be sad
-    MAP_I2CSlaveEnable(I2C_MODULE);
-    MAP_I2CSlaveInit(I2C_MODULE, I2C_ADDRESS);
     // Register IRQ and clear spurious conditions
+    MAP_IntEnable(INT_I2C1);
     MAP_I2CSlaveIntClear(I2C_MODULE);
     MAP_I2CSlaveIntEnableEx(I2C_MODULE, I2C_SLAVE_INT_DATA);
+    // Set up in slave mode with correct address
+    // NOTE TM4C123 does not have slave functions in its ROM, do not try, you will be sad
+    MAP_I2CMasterDisable(I2C_MODULE);
+    MAP_I2CSlaveEnable(I2C_MODULE);
+    MAP_I2CSlaveInit(I2C_MODULE, I2C_ADDRESS);
     // Init register file
     i2cData.address = 0U;
     memset(i2cData.regs, 0, sizeof(i2cData.regs));

gps-radio-tiva-c/src/i2c.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "nmea_messages.h"
+#include "telemetry.h"
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -10,7 +11,7 @@
 // Our software version, major (API compatible)
 #define SW_VERSION_MAJOR 2
 // Our software version, minor (revision)
-#define SW_VERSION_MINOR 0
+#define SW_VERSION_MINOR 1
 
 // I2C module to use
 // NOTE If I2C_MODULE is changed, check initializeI2C to update pin mappings/clocks!
@@ -22,6 +23,10 @@
 #define REG_SW_VERSION_MAJOR 0x01
 #define REG_SW_VERSION_MINOR 0x02
 #define REG_DATA_AVAILABLE 0x03
+#define REG_TEMP_0 0x04
+#define REG_TEMP_1 0x05
+#define REG_VOLT_0 0x06
+#define REG_VOLT_1 0x07
 #define REG_LAT_0 0x00
 #define REG_LAT_1 0x01
 #define REG_LAT_2 0x02
@@ -50,3 +55,5 @@ void initializeI2C(void);
 // Submits parsed GPS data to the I2C subsystem
 // index is the GPS (0 = Venus, 1 = Copernicus) to update
 void submitI2CData(uint32_t index, GpsData *data);
+// Submits voltage and temperature data to the I2C subsystem
+void submitI2CTelemetry(Telemetry *telemetry);

gps-radio-tiva-c/src/main.c
@@ -12,14 +12,21 @@
 #include <driverlib/rom.h>
 #include <driverlib/systick.h>
 
+// Reduce stack usage by main() and get a "free" zero initialization!
+static GpsData venusGpsData;
+static GpsData copernicusGpsData;
+static Message venusGpsMessage;
+static Message copernicusGpsMessage;
+static Telemetry telemetry;
+
 int main()
 {
     initializeTivaC();
     initializeAprs();
     initializeTimer();
     initializeUart();
-    initializeI2C();
     initializeSignals();
+    initializeI2C();
     initializeTelemetry();
 
     bool r = true;
@@ -29,6 +36,26 @@ int main()
 #ifdef DEBUG
     r &= initializeUartChannel(CHANNEL_DEBUG, UART_0, 115200, CPU_SPEED, UART_FLAGS_SEND);
 #endif
+    
+    // AIR mode for Copernicus:
+    // 0x10 DLE
+    // 0xBB Set GPS Processing Options
+    // 0x03 Query Mode
+    // 0x00 Auto 2D/3D
+    // 0x00 Reserved
+    // 0x03 Dynamics Mode AIR
+    // 0x00 Reserved
+    //  0x3db2b8c2 = 5 degrees in radians little endian (0.087266), Copernicus is big endian
+    // 0xC2 Lowest satellite elevation angle for fix
+    // 0xB8
+    // 0xB2
+    // 0x3D
+    //  0x3f19999a = 0.6 little endian
+    // 0x9A Minimum signal level for fix
+    // 0x99
+    // 0x19
+    // 0x3F
+    // 0x00 * 27 (4 + 4 + 1 + 18) Reserved
 
     if (r)
     {
@@ -38,14 +65,6 @@ int main()
     {
         signalError();
     }
-
-    GpsData venusGpsData = { 0 };
-    GpsData copernicusGpsData = { 0 };
-
-    Message venusGpsMessage = { 0 };
-    Message copernicusGpsMessage = { 0 };
-
-    Telemetry telemetry;
 
     bool shouldSendVenusDataToAprs = true;
 
@@ -103,6 +122,7 @@ int main()
         if (currentTime - lastRadioSentTime >= RADIO_MCU_MESSAGE_SENDING_INTERVAL_SECONDS)
         {
             getTelemetry(&telemetry);
+            submitI2CTelemetry(&telemetry);
             
             if (shouldSendVenusDataToAprs && venusGpsData.isValid)
             {

gps-radio-tiva-c/src/signals.c
@@ -7,28 +7,36 @@
 #include <driverlib/pwm.h>
 #include <driverlib/rom.h>
 #include <driverlib/gpio.h>
+#include <driverlib/pin_map.h>
 #include <driverlib/sysctl.h>
 
-// #define PWM_OUTPUT
+// Enables dimmable PWM output on the LEDs
+#define PWM_OUTPUT
 
 // Configures the PWM generator to the right values
 #define PWM_GEN_CONFIGURE(_gen) do {\
-    ROM_PWMGenConfigure(PWM1_BASE, (_gen), PWM_GEN_MODE_SYNC | PWM_GEN_MODE_DOWN |\
+    ROM_PWMGenConfigure(PWM1_BASE, (_gen), PWM_GEN_MODE_NO_SYNC | PWM_GEN_MODE_UP_DOWN |\
         PWM_GEN_MODE_DBG_RUN | PWM_GEN_MODE_GEN_NO_SYNC);\
-    ROM_PWMGenEnable(PWM1_BASE, (_gen));\
     ROM_PWMGenPeriodSet(PWM1_BASE, (_gen), 0xFFFFU);\
+    ROM_PWMGenEnable(PWM1_BASE, (_gen));\
 } while (0)
 
 void initializeSignals(void)
 {
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    ROM_GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD);
 #ifdef PWM_OUTPUT
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
     ROM_GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-    PWMClockSet(PWM1_BASE, PWM_SYSCLK_DIV_1);
+    ROM_GPIOPinConfigure(GPIO_PF1_M1PWM5);
+    ROM_GPIOPinConfigure(GPIO_PF2_M1PWM6);
+    ROM_GPIOPinConfigure(GPIO_PF3_M1PWM7);
+    // Do not use with TM4C123 devices (datasheet p. 420)
+    //PWMClockSet(PWM1_BASE, PWM_SYSCLK_DIV_1);
     // Need to set up Generator 2 (M1PWM5=R) and Generator 3 (M1PWM6=B, M1PWM7=G)
     PWM_GEN_CONFIGURE(PWM_GEN_2);
     PWM_GEN_CONFIGURE(PWM_GEN_3);
+    ROM_PWMOutputInvert(PWM1_BASE, PWM_OUT_5_BIT | PWM_OUT_6_BIT | PWM_OUT_7_BIT, false);
     ROM_PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT | PWM_OUT_6_BIT | PWM_OUT_7_BIT, true);
 #else
     ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
@@ -36,14 +44,33 @@ void initializeSignals(void)
     signalOff();
 }
 
+// The rest of these functions should be self explanatory
+
+#ifdef PWM_OUTPUT
+void signalRed(const uint32_t value)
+{
+    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5, value);
+}
+
+void signalGreen(const uint32_t value)
+{
+    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7, value);
+}
+
+void signalBlue(const uint32_t value)
+{
+    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6, value);
+}
+#endif
+
 void signalOff(void)
 {
 #ifdef PWM_OUTPUT
-    signalRed(0);
-    signalGreen(0);
-    signalBlue(0);
+    signalRed(0U);
+    signalGreen(0U);
+    signalBlue(0U);
 #else
-	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0U);
+	ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0U);
 #endif
 }
 
@@ -53,7 +80,7 @@ void signalSuccess(void)
     signalRed(0U);
     signalGreen(1024U);
 #else
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, GPIO_PIN_3);
+    ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, GPIO_PIN_3);
 #endif
 }
 
@@ -63,7 +90,7 @@ void signalError(void)
     signalRed(4096U);
     signalGreen(0U);
 #else
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, GPIO_PIN_1);
+    ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, GPIO_PIN_1);
 #endif
 }
 
@@ -71,26 +98,26 @@ void signalFaultInterrupt(void)
 {
 #ifdef PWM_OUTPUT
     signalRed(4096U);
-    signalGreen(0U);
+    signalGreen(1024U);
 #else
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_1 | GPIO_PIN_3);
+    ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_1 | GPIO_PIN_3);
 #endif
 }
 
 void signalI2CDataRequested()
 {
 #ifdef PWM_OUTPUT
-    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6, 0xFFFFU);
+    signalBlue(0xFFFFU);
 #else
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+    ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
 #endif
 }
 
 void clearI2CDataRequested()
 {
 #ifdef PWM_OUTPUT
-    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6, 0x0U);
+    signalBlue(0U);
 #else
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+    ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
 #endif
 }

gps-radio-tiva-c/src/signals.h
@@ -11,3 +11,8 @@ void signalFaultInterrupt(void);
 
 void signalI2CDataRequested(void);
 void clearI2CDataRequested(void);
+
+// PWM mode only!
+void signalRed(const uint32_t value);
+void signalGreen(const uint32_t value);
+void signalBlue(const uint32_t value);

gps-radio-tiva-c/src/timer.c
@@ -9,7 +9,7 @@
 #include <driverlib/timer.h>
 #include <driverlib/sysctl.h>
 
-uint32_t timerSeconds;
+uint32_t timerSeconds = 0;
 
 void initializeTimer(void)
 {